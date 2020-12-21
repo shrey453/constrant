c4dcb6a48b1545711813ff55a59f77523c4f4ae1
shrey453 <75714339+shrey453@users.noreply.github.com>
2020-12-21 20:58:50 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,20 @@
+class Chain {
+    constructor(bodyA,bodyB){
+        var options={
+            bodyA:bodyA,
+            bodyB:bodyB,
+            stiffness:0.4,
+            length:10
+            
+        }
+       this.chain = Constraint.create(options)
+       World.add(world,this.chain)
+       
+    }
+    display(){
+        push();
+        strokeWeight(3);
+        line(this.chain.bodyA.position.x,this.chain.bodyA.position.y,this.chain.bodyB.position.x,this.chain.bodyB.position.y)
+        pop();
+    }
+}
